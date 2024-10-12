db2273d2e3e786e7862c6c645b24fddf226f644f
nuharaomesh <omeshnuhara4@gmail.com>
2024-10-12 12:38:32 +0530

    update project

--- src/main/java/com/example/add_spring_boot/config/SecurityConfig.java
+++ src/main/java/com/example/add_spring_boot/config/SecurityConfig.java
@@ -1,35 +1,35 @@
 package com.example.add_spring_boot.config;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception{
-        http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests()
-                .anyRequest().authenticated() // Authenticating all requests
-                .and()
-                .httpBasic();
-
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, HttpSession httpSession) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)                  // Approve all requests that match to this
+                .authorizeHttpRequests(req -> req.requestMatchers("api/v1/auth/**")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider()
+                .addFilterBefore();
         return http.build();
     }
 
-    @Bean
-    public InMemoryUserDetailsManager manager() {
-        UserDetails principleUser = User.withDefaultPasswordEncoder() // Principle User
-                .username("Omesh")
-                .password("1234")
-                .roles("USER") //Define the User Role
-                .build();
-        return new InMemoryUserDetailsManager(principleUser);
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider dap =
+                new DaoAuthenticationProvider();
+        dap.setUserDetailsService();
     }
 }
