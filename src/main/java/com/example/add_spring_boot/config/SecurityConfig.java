9f52744fe38abeae9b0bbd636ac83b633c3f918c
nuharaomesh <omeshnuhara4@gmail.com>
2024-10-12 10:35:58 +0530

    config Basic Auth

--- src/main/java/com/example/add_spring_boot/config/SecurityConfig.java
+++ src/main/java/com/example/add_spring_boot/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.add_spring_boot.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,6 +12,13 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Value("${secure.username}")
+    private String userName;
+    @Value("${secure.password}")
+    private String password;
+    @Value("${secure.role}")
+    private String role;
 
     @Bean
     SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception{
@@ -26,9 +34,9 @@ public class SecurityConfig {
     @Bean
     public InMemoryUserDetailsManager manager() {
         UserDetails principleUser = User.withDefaultPasswordEncoder() // Principle User
-                .username("Omesh")
-                .password("1234")
-                .roles("USER") //Define the User Role
+                .username(userName)
+                .password(password)
+                .roles(role) // Define the User Role
                 .build();
         return new InMemoryUserDetailsManager(principleUser);
     }
